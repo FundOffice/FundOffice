4d0769db65857a4e9e33accf0f40e2182ff53563
k1k <k1k@seesunfund.com>
2026-04-23 15:51:26 +0800

    add AutoViewModel 标签

---
+++ src/Main/Models/AutoViewModelAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMO.Models;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class AutoViewModelAttribute : Attribute
+{
+    public Type SourceType { get; }
+
+    public AutoViewModelAttribute(Type sourceType)
+    {
+        SourceType = sourceType;
+    }
+}
