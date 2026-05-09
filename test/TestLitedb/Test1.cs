88ab2db64af1aac1ea4a3964687f927103c0dfbe
iyu <iyumot@163.com>
2026-05-09 17:16:37 +0800

    test

--- test/TestLitedb/Test1.cs
+++ test/TestLitedb/Test1.cs
@@ -2,12 +2,31 @@
 using FMO.Utilities;
 using LiteDB;
 using Microsoft.Win32;
+using System.Diagnostics;
+using System.Linq.Expressions;
 
 namespace TestLitedb;
 
 [TestClass]
 public sealed class Test1
 {
+    public Test1()
+    {
+        using (var key = Registry.CurrentUser.OpenSubKey(@$"Software\Nexus\Debug"))
+        {
+            if (key != null)
+            {
+                var workFolder = key.GetValue("WorkingFolder") as string;
+                if (!string.IsNullOrWhiteSpace(workFolder))
+                {
+                    var di = new DirectoryInfo(workFolder);
+                    if (di.Exists)
+                        Directory.SetCurrentDirectory(di.FullName);
+                }
+            }
+        }
+    }
+
     [TestMethod]
     public void TestMethod1()
     {
@@ -28,23 +47,11 @@ public sealed class Test1
     [TestMethod]
     public void TestFact()
     {
-        using (var key = Registry.CurrentUser.OpenSubKey(@$"Software\Nexus\Debug")) 
-        {
-            if (key != null)
-            {
-                var workFolder = key.GetValue("WorkingFolder") as string;
-                if (!string.IsNullOrWhiteSpace(workFolder))
-                {
-                    var di = new DirectoryInfo(workFolder);
-                    if (di.Exists)
-                        Directory.SetCurrentDirectory(di.FullName);
-                }
-            }
-        }
+
         using var db = DbHelper.Base();
-        var ele =  db.GetCollection<FundElements>().FindById(9);
+        var ele = db.GetCollection<FundElements>().FindById(9);
         var facts = FundElementsFundFactHelper.FromElement(ele);
-                 
+
         using var db2 = new LiteDatabase("FileName=xx.db");
         //db2.DropCollection(nameof(IFundFact));
         db2.GetCollection<IFundFact>().Upsert(facts);
@@ -56,6 +63,47 @@ public sealed class Test1
 
         var nele = FundElementsFundFactHelper.ToElement(facts, 9);
     }
+
+
+
+    [TestMethod]
+    public void TestLinq()
+    {
+        using var db = DbHelper.Base();
+
+        Expression<Func<ILiteCollection<Fund>, object>> expression = x => x.FindAll().ToArray();
+
+
+        Serialize.Linq.Serializers.ExpressionSerializer serializer = new Serialize.Linq.Serializers.ExpressionSerializer(new Serialize.Linq.Serializers.JsonSerializer());
+
+        var text = serializer.SerializeText(expression);
+
+        var ex = serializer.DeserializeText(text) as Expression<Func<ILiteCollection<Fund>, object>>;
+
+        var obj = ex.Compile().Invoke(db.GetCollection<Fund>());
+
+    }
+
+    [TestMethod]
+    public void MyTestMethod()
+    {
+
+
+        var typen = $"{typeof(LiquidationFlow).FullName},{typeof(LiquidationFlow).Assembly.GetName().Name}";
+
+        Debug.WriteLine(typen);
+
+        using var db = DbHelper.Base();
+        var types = db.GetCollection(nameof(FundFlow)).FindAll().ToArray();
+
+        foreach (var item in types)
+        {
+            Debug.WriteLine(item["_type"]);
+        }
+
+    }
+
+
 }
 
 
