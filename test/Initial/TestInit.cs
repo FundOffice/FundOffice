acefcc2c6921e62897e0a893a787296bbd0f2e34
k1k <k1k@seesunfund.com>
2026-05-12 14:01:22 +0800

    test

---
+++ test/Initial/TestInit.cs
@@ -0,0 +1,47 @@
+﻿using FMO.Utilities;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+
+namespace Initial;
+
+#pragma warning disable CA1416 // 验证平台兼容性
+public class TestInit
+{
+    public static void SetAsDebug()
+    {
+
+        using (var key = Registry.CurrentUser.CreateSubKey(@$"Software\Nexus\Debug"))
+        {
+            if (key.GetValue("WorkingFolder") is string dir)
+            {
+                Directory.SetCurrentDirectory(dir);
+
+                Debug.WriteLine("=====================================================");
+
+                Debug.WriteLine($"当前工作目录{dir}");
+            }
+            DbHelper.Init();
+            Debug.WriteLine("=====================================================");
+        }
+    }
+
+    public static void SetAsRelease()
+    {
+
+        using (var key = Registry.CurrentUser.CreateSubKey(@$"Software\Nexus"))
+        {
+            if (key.GetValue("WorkingFolder") is string dir)
+            {
+                Directory.SetCurrentDirectory(dir);
+
+                Debug.WriteLine("=====================================================");
+
+                Debug.WriteLine($"当前工作目录{dir}");
+            }
+            DbHelper.Init();
+            Debug.WriteLine("=====================================================");
+        }
+    }
+}
+#pragma warning restore CA1416 // 验证平台兼容性
---
+++ test/TestMeishi/MeishiTest.cs
@@ -0,0 +1,71 @@
+﻿using FMO.Models;
+using FMO.Utilities;
+using Initial;
+using System.Text.Json.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TestMeishi;
+
+[TestClass]
+public sealed class MeishiTest
+{
+    public MeishiTest()
+    {
+        TestInit.SetAsDebug();
+    }
+        
+    [TestMethod]
+    public async Task CreateTemporaryOpenDay_FundNotFound()
+    { 
+
+        var mei = new FMO.ESigning.MeiShi.MeiShiAssit();
+
+        var date = DateOnly.FromDateTime(DateTime.Now);
+        var result = await mei.CreateTemporaryOpenDay(2, null, date.AddDays(6), OpenFlag.Buy, true);
+
+       
+    }
+
+    [TestMethod]
+    public void TestTimeRange()
+    {
+        Console.WriteLine(new TimeRange(new(2026, 5, 12)));
+    }
+
+    [TestMethod]
+    public void TestNotice()
+    {
+        var date = DateOnly.FromDateTime(DateTime.Now).AddDays(5);
+        var d = Math.Max(0, date.DayNumber - DateOnly.FromDateTime(DateTime.Now).DayNumber);
+
+
+        Console.WriteLine(d);
+    }
+}
+
+
+internal class TimeRange
+{
+    public TimeRange(DateOnly date)
+    {
+        StartTime = new DateTime(date, default).ToUniversalTime().TimeStampByMilliseconds();
+        EndTime = StartTime + 86399000;
+    }
+
+    /// <summary>
+    /// 开始时间戳（毫秒）
+    /// </summary>
+    [JsonPropertyName("startTime")]
+    public long StartTime { get; set; }
+
+    /// <summary>
+    /// 结束时间戳（毫秒）
+    /// </summary>
+    [JsonPropertyName("endTime")]
+    public long EndTime { get; set; }
+
+    public override string ToString()
+    {
+        return $"{StartTime} {EndTime}";
+    }
+}
