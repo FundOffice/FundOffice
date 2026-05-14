c121da433a4a49428012c46d7a8619007d1cb939
iyumot <iyumot@163.com>
2026-05-14 21:14:31 +0800

    add toast 简化 WeakReferenceMessenger.Default.Send(new ToastMessage(LogLevel.Info, message));

---
+++ src/Utility/Utilities/Toast.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using FMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utilities;
+
+public class Toast
+{
+
+    /// <summary>
+    /// 普通信息提示
+    /// </summary>
+    /// <param name="message">提示内容</param>
+    public static void Info(string message) =>
+        WeakReferenceMessenger.Default.Send(new ToastMessage(LogLevel.Info, message));
+
+    /// <summary>
+    /// 成功提示
+    /// </summary>
+    /// <param name="message">提示内容</param>
+    public static void Success(string message) =>
+        WeakReferenceMessenger.Default.Send(new ToastMessage(LogLevel.Success, message));
+
+    /// <summary>
+    /// 警告提示
+    /// </summary>
+    /// <param name="message">提示内容</param>
+    public static void Warning(string message) =>
+        WeakReferenceMessenger.Default.Send(new ToastMessage(LogLevel.Warning, message));
+
+    /// <summary>
+    /// 错误提示
+    /// </summary>
+    /// <param name="message">提示内容</param>
+    public static void Error(string message) =>
+        WeakReferenceMessenger.Default.Send(new ToastMessage(LogLevel.Error, message));
+}
