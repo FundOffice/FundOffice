64b0db2f0c637d83e4be180f7819b09b56b67394
k1k <k1k@seesunfund.com>
2026-05-15 17:08:14 +0800

    add aes 加密byte[]

--- src/Utility/Utilities/AesHelper.cs
+++ src/Utility/Utilities/AesHelper.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace Utilities;
 
 public static class AesHelper
 {
- 
-    private static readonly string _aesSecret = "OHM5a0YycFI3eFEzZEc1akwxekM0dkI2bk0wYVM3d0V0TjViUjhnSzJtUDRxWDd6"; 
+
+    private static readonly string _aesSecret = "OHM5a0YycFI3eFEzZEc1akwxekM0dkI2bk0wYVM3d0V0TjViUjhnSzJtUDRxWDd6";
 
     /// <summary>
     /// AES 加密字符串
@@ -21,7 +19,7 @@ public static class AesHelper
             return string.Empty;
 
         var bytes = Convert.FromBase64String(_aesSecret);
-         
+
         byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
 
         using (Aes aes = Aes.Create())
@@ -69,6 +67,67 @@ public static class AesHelper
                 cs.Write(cipherBytes, 0, cipherBytes.Length);
                 cs.FlushFinalBlock();
                 return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// AES 加密字符串
+    /// </summary>
+    /// <param name="plainText">明文</param>
+    /// <returns>Base64 加密结果</returns>
+    public static byte[] Encrypt(byte[] plainBytes)
+    {
+        if (plainBytes?.Length is null or 0)
+            return [];
+
+        var bytes = Convert.FromBase64String(_aesSecret);
+
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = bytes[..32];
+            aes.IV = bytes[32..];
+            // 加密模式 & 填充模式（通用标准）
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(plainBytes, 0, plainBytes.Length);
+                cs.FlushFinalBlock();
+                return ms.ToArray();
+            }
+        }
+    }
+
+    /// <summary>
+    /// AES 解密字符串
+    /// </summary>
+    /// <param name="cipherText">Base64 加密串</param>
+    /// <returns>明文</returns>
+    public static byte[] Decrypt(byte[] cipherBytes)
+    {
+        if (cipherBytes?.Length is null or 0)
+            return [];
+
+        var bytes = Convert.FromBase64String(_aesSecret);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = bytes[..32];
+            aes.IV = bytes[32..];
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(cipherBytes, 0, cipherBytes.Length);
+                cs.FlushFinalBlock();
+                return ms.ToArray();
             }
         }
     }
