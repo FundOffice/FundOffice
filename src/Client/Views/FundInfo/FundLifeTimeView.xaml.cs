10f7a62c43aa00a2a436b8c935302bc230182c8d
iyumot <iyumot@163.com>
2026-06-03 20:36:38 +0800

    调整ui

--- src/Client/Views/FundInfo/FundLifeTimeView.xaml.cs
+++ src/Client/Views/FundInfo/FundLifeTimeView.xaml.cs
@@ -19,56 +19,6 @@ public partial class FundLifeTimeView : UserControl
 
 
 
-public class FlowTemplateSelector : DataTemplateSelector
-{
-    public override DataTemplate SelectTemplate(object item, DependencyObject container)
-    {
-        if (container is not FrameworkElement element)
-            return base.SelectTemplate(item, container);
-
-        switch (item)
-        {
-            case InitiateFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.InitiateFlow");
-
-
-            case ContractModifyFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.ContractModifyFlow");
-
-
-            case ContractFinalizeFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.ContractFinalizeFlow");
-
-
-            case SetupFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.SetupFlow");
-
-            case RegistrationFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.Registration");
-
-
-            case ModifyByAnnounceFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.ModifyByAnnounceFlow");
-
-
-
-            case LiquidationFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.Liquidation");
-
-            case DividendFlowViewModel vm:
-                return (DataTemplate)element.TryFindResource("DT.DividendFlow");
-
-            default:
-                break;
-        }
-
-
-
-
-        return base.SelectTemplate(item, container);
-    }
-}
-
 
 public partial class CustomFileInfoViewModel : ObservableObject
 {
