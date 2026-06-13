58a30598f4bb0431586c4fd1578f71857ced03ae
iyu <iyumot@163.com>
2026-06-13 21:57:58 +0800

    支持枚举描述转换及模板绑定优化
    修改 `EnumDescriptionTypeConverter` 支持 `[Flags]` 枚举描述拼接，提取私有方法 `GetSingleFieldDescription` 并优化描述获取逻辑，新增重载方法支持按类型和字段名获取描述。
    
    新增 `ObjectDisplayConverter` 类，实现枚举描述转换逻辑，并在 `Modifiable.xaml` 中引入，优化默认只读模板的绑定显示。

---
+++ src/SharedUI/ObjectDesplayConverter.cs
@@ -0,0 +1,22 @@
+﻿using FMO.Models;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FMO.Shared;
+
+internal class ObjectDisplayConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            Enum e => EnumDescriptionTypeConverter.GetEnumDescription(e),
+            _ => value?.ToString() ?? ""
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
